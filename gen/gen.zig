2775ca88eb1cce6b0d1b1e734fa92b94ce8725e0
Jerzy Wilczek <jerzykwilczek@gmail.com>
2024-11-18 23:51:56 +0100

    WIP

--- gen/gen.zig
+++ gen/gen.zig
@@ -2,44 +2,111 @@ const std = @import("std");
 const xml = @import("xml");
 
 pub fn main() !void {
-    var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena_state.deinit();
-    const arena = arena_state.allocator();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
 
-    const args = try std.process.argsAlloc(arena);
+    const args = try std.process.argsAlloc(allocator);
     if (args.len != 3) {
-        std.debug.print("wrong number of arguments", .{});
+        std.debug.print("wrong number of arguments\n", .{});
         std.process.exit(1);
     }
+    defer std.process.argsFree(allocator, args);
 
     const input_file_path = args[1];
     var input_file = std.fs.cwd().openFile(input_file_path, .{}) catch |err| {
-        std.debug.print("cannot open file '{s}': {s}", .{ input_file_path, @errorName(err) });
+        std.debug.print("cannot open file '{s}': {s}\n", .{ input_file_path, @errorName(err) });
         std.process.exit(1);
     };
     defer input_file.close();
     var buf_reader = std.io.bufferedReader(input_file.reader());
     var input_reader = buf_reader.reader();
     const max_xml_size = 1024 * 1024 * 1024;
-    const input_contents = input_reader.readAllAlloc(arena, max_xml_size) catch |err| {
+    const input_contents = input_reader.readAllAlloc(allocator, max_xml_size) catch |err| {
         switch (err) {
-            error.StreamTooLong => std.debug.print("xml longer than {d}", .{max_xml_size}),
-            else => std.debug.print("cannot read file: {s}", .{@errorName(err)}),
+            error.StreamTooLong => std.debug.print("xml longer than {d}\n", .{max_xml_size}),
+            else => std.debug.print("cannot read file: {s}\n", .{@errorName(err)}),
         }
         std.process.exit(1);
     };
+    defer allocator.free(input_contents);
 
     const output_file_path = args[2];
     var output_file = std.fs.cwd().createFile(output_file_path, .{}) catch |err| {
-        std.debug.print("cannot create file '{s}': {s}", .{ output_file_path, @errorName(err) });
+        std.debug.print("cannot create file '{s}': {s}\n", .{ output_file_path, @errorName(err) });
         std.process.exit(1);
     };
     defer output_file.close();
 
-    const parsed_file = try xml.parse(arena, input_contents);
-    _ = parsed_file;
+    const parsed_file = try xml.parse(allocator, input_contents);
+    defer parsed_file.deinit();
 
-    try output_file.writeAll(
-        \\pub const test_message: []const u8 = "test";
-    );
+    try typegenDocument(&parsed_file, output_file.writer());
+}
+
+fn typegenDocument(document: *const xml.Document, writer: std.fs.File.Writer) !void {
+    if (std.mem.eql(u8, document.root.tag, "protocol")) {
+        for (document.root.children) |child| {
+            switch (child) {
+                .comment => {},
+                .char_data => |data| if (!std.mem.eql(u8, std.mem.trim(u8, data, " \t\n\r"), "")) {
+                    std.log.warn("Found unexpected char data in the 'protocol' element ({s}).\n", .{data});
+                },
+                .element => |element| if (std.mem.eql(u8, element.tag, "interface")) {
+                    try typegenInterface(element, writer);
+                } else if (std.mem.eql(u8, element.tag, "copyright")) {} else {
+                    std.log.warn("Found unexpected child of the 'protocol' element with tag {s}", .{element.tag});
+                },
+            }
+        }
+    } else {
+        return error.NoProtocol;
+    }
+}
+
+fn typegenInterface(interface: *xml.Element, writer: std.fs.File.Writer) !void {
+    std.debug.print("interface: {s}\n", .{interface.getAttribute("name").?});
+    _ = writer;
+}
+
+const PrintParams = struct {
+    indent: usize = 0,
+};
+
+fn printElement(element: *xml.Element, params: PrintParams, alloc: std.mem.Allocator) !void {
+    var indent_builder = std.ArrayList(u8).init(alloc);
+    try indent_builder.appendNTimes(' ', params.indent);
+    const indent = try indent_builder.toOwnedSlice();
+    defer alloc.free(indent);
+
+    std.debug.print("{s}<{s}", .{ indent, element.tag });
+    for (element.attributes) |attr| {
+        std.debug.print(" {s}=\"{s}\"", .{ attr.name, attr.value });
+    }
+    std.debug.print(">\n", .{});
+
+    for (element.children) |child| {
+        switch (child) {
+            .element => |el| try printElement(el, .{ .indent = params.indent + 2 }, alloc),
+            .comment => {},
+            .char_data => |data| try printChars(data, .{ .indent = params.indent + 2 }, alloc),
+        }
+    }
+
+    std.debug.print("{s}</{s}>\n", .{ indent, element.tag });
+}
+
+fn printChars(str: []const u8, params: PrintParams, alloc: std.mem.Allocator) !void {
+    var indent_builder = std.ArrayList(u8).init(alloc);
+    try indent_builder.appendNTimes(' ', params.indent);
+    const indent = try indent_builder.toOwnedSlice();
+    defer alloc.free(indent);
+
+    var iter = std.mem.splitSequence(u8, str, "\n");
+    while (iter.next()) |line| {
+        const trimmed = std.mem.trim(u8, line, " \t\n\r");
+        if (trimmed.len > 0) {
+            std.debug.print("{s}{s}\n", .{ indent, trimmed });
+        }
+    }
 }
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,122 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const Allocator = std.mem.Allocator;
 const wayland = @import("wayland");
 
+fn headerFromMessage(message: anytype, object_id: u32) MessageHeader {
+    var len = @as(u16, @sizeOf(MessageHeader));
+    inline for (std.meta.fields(@TypeOf(message))) |field| {
+        switch (field.type) {
+            u32 => len += 4,
+            i32 => len += 4,
+            else => @compileError("Cannot create a MessageHeader from " ++ @typeName(@TypeOf(message)) ++ ", field \"" ++ field.name ++ "\" has a wrong type."),
+        }
+    }
+
+    return MessageHeader{
+        .object_id = object_id,
+        .opcode = @TypeOf(message).opcode,
+        .size = len,
+    };
+}
+
+const MessageHeader = if (builtin.cpu.arch.endian() == std.builtin.Endian.little)
+    packed struct {
+        object_id: u32,
+        opcode: u16,
+        size: u16,
+    }
+else
+    packed struct {
+        object_id: u32,
+        size: u16,
+        opcode: u16,
+    };
+
+const Interface = enum {
+    wl_display,
+    wl_callback,
+    wl_registry,
+};
+
+const InterfaceRegistry = struct {
+    const InterfaceMap = std.AutoHashMap(u32, Interface);
+    map: InterfaceMap,
+
+    fn init(alloc: Allocator) InterfaceRegistry {
+        return InterfaceRegistry{
+            .map = InterfaceMap.init(alloc),
+        };
+    }
+
+    fn deinit(self: *InterfaceRegistry) void {
+        self.map.deinit();
+    }
+
+    fn put(self: *InterfaceRegistry, id: u32, interface: Interface) !void {
+        try self.map.put(id, interface);
+    }
+};
+
+const WlDisplay = struct {
+    const version = 1;
+    id: u32,
+
+    const SyncParams = struct {
+        const opcode = 0;
+        callback: u32,
+    };
+
+    const GetRegistryParams = struct {
+        const opcode = 1;
+        registry: u32,
+    };
+
+    fn getRegistry(self: *const WlDisplay, params: GetRegistryParams, writer: anytype, registry: *InterfaceRegistry) !WlRegistry {
+        try sendMessage(writer, params, self.id);
+        try registry.put(params.registry, Interface.wl_registry);
+        return WlRegistry{ .id = params.registry };
+    }
+};
+
+const WlRegistry = struct {
+    const version = 1;
+    id: u32,
+};
+
+fn sendMessage(writer: anytype, message: anytype, object_id: u32) !void {
+    const endianness = builtin.cpu.arch.endian();
+    const header = headerFromMessage(message, object_id);
+    try writer.writeStruct(header);
+    inline for (std.meta.fields(@TypeOf(message))) |field| {
+        switch (field.type) {
+            u32 => try writer.writeInt(u32, @field(message, field.name), endianness),
+            i32 => try writer.writeInt(i32, @field(message, field.name), endianness),
+            else => @compileError("Cannot send a message with struct " ++ @typeName(@TypeOf(message)) ++ ", field \"" ++ field.name ++ "\" has a wrong type."),
+        }
+    }
+}
+
+fn readResponse() !void {}
+
 pub fn main() !void {
-    std.debug.print("test message: {s}", .{wayland.test_message});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const alloc = gpa.allocator();
+
+    const xdg_runtime_dir = std.posix.getenv("XDG_RUNTIME_DIR") orelse return error.NoXdgRuntimeDir;
+    const wayland_display = std.posix.getenv("WAYLAND_DISPLAY") orelse return error.NoWaylandDisplay;
+    const wayland_socket_path = try std.fs.path.joinZ(alloc, &.{ xdg_runtime_dir, wayland_display });
+    defer alloc.free(wayland_socket_path);
+    const socket = try std.net.connectUnixSocket(wayland_socket_path);
+
+    const socket_writer = socket.writer();
+    const socket_reader = socket.reader()
+    const wl_display = WlDisplay{ .id = 1 };
+    var interface_registry = InterfaceRegistry.init(alloc);
+    defer interface_registry.deinit();
+    try interface_registry.put(1, Interface.wl_display);
+
+    const wl_registry = try wl_display.getRegistry(.{ .registry = 2 }, socket_writer, &interface_registry);
+    _ = wl_registry;
 }
